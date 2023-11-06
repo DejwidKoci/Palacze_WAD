05defe975335cb2bad9ffee21f705ca0a02ef7ae
Dejwid <s205915@sggw.edu.pl>
2023-11-06 18:02:15 +0100

    metoda warda

---
+++ ward.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from scipy.cluster.hierarchy import dendrogram, linkage
+from matplotlib import pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+
+data = pd.read_excel('Data.xlsx', index_col = 0)
+data.fillna(0, inplace=True)
+
+data_norm = (data - data.mean())/ data.std()
+dist_matrix = linkage(data_norm, 'ward')
+
+plt.figure(figsize=(10, 7))
+plt.title("Metoda Warda")
+dend = dendrogram(dist_matrix, labels = data.index, orientation = 'right')
+plt.show()
